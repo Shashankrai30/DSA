36d23c0dfde89cf0de633c9f5f4be6a518ef5bdf
Shashankrai30 <164053591+Shashankrai30@users.noreply.github.com>
2024-08-23 15:32:06 +0530

    Add files via upload

---
+++ addingmatrix.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class addingmatrix{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x,y;
+        System.out.print("Enter the number of rows : ");
+        x=sc.nextInt();
+        System.out.print("Enter the number of columns : ");
+        y=sc.nextInt();
+        int arr[][]=new int[x][y];
+        int arr1[][]=new int[x][y];
+        int arr2[][]=new int[x][y];
+        System.out.println("Matrix 1 : ");
+        for(int i=0;i<x;i++){
+            System.out.println("Row "+(i+1)+": ");
+            for(int j=0;j<y;j++){
+                System.out.print("Enter the value : ");
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Matrix 2 : ");
+        for(int i=0;i<x;i++){
+            System.out.println("Row "+(i+1)+": ");
+            for(int j=0;j<y;j++){
+                System.out.print("Enter the value : ");
+                arr1[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                arr2[i][j]=arr[i][j]+arr1[i][j];
+            }
+        }
+        System.out.println("Addition of Matrix 1 and Matrix 2 : ");
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                System.out.print(arr2[i][j]+"  ");
+                }
+                System.out.println();
+        }
+        sc.close();
+    }
+}
