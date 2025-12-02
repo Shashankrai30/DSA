9a6cb51d64560c6aac07427a85ce588927f31ee6
Shashank Rai <raishashank1462@gmail.com>
2025-12-02 09:29:52 +0530

    first commit

--- DP/fabonacci.java
+++ DP/fabonacci.java
@@ -1,4 +1,20 @@
 package DP;
+import java.util.*;
 
 public class fabonacci {
+    public static void main(String[] args) {
+        int n=5;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(fib(n,dp));
+    }
+    public static int fib(int n,int dp[]){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+    }
 }
